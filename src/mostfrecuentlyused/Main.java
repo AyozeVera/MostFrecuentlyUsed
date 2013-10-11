c130ff7418316b1ca8b73e94f03d0b585ac9d2a9
Ayoze <Ayoze@AyoVera-PC>
2013-10-11 12:30:35 +0100

    Third commit

--- src/mostfrecuentlyused/Main.java
+++ src/mostfrecuentlyused/Main.java
@@ -7,7 +7,7 @@ int[]v={2, 3, 5 , 1, 4, 5, 3, 2, 4, 1, 1, 1};
 MostFrecuentlyUsed array = new MostFrecuentlyUsed(v); 
 
     public static void main(String[] args) {
-        System.out.println((array.getFrecuentlyUsed().toString());
+        System.out.println(array.getFrecuentlyUsed());
         
     }
 }
